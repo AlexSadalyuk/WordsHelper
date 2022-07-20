f7625216d44e86816db003da84bb70a3512132be
Oleksandr <sadalyukalex@gmail.com>
2022-07-20 16:24:58 +0300

    clean up

--- Client/src/app/models/consts.ts
+++
@@ -1,6 +0,0 @@
-const settings =
-{
-    domain: "http://words-helper.somee.com"
-};
-
-export default settings;
--- Client/src/app/models/translate.spec.ts
+++
@@ -1,7 +0,0 @@
-import { Translate } from './translate';
-
-describe('Translate', () => {
-  it('should create an instance', () => {
-    expect(new Translate()).toBeTruthy();
-  });
-});
